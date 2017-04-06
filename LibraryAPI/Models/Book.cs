575fdb5957e5e0fdff2e837ef971525c798deefe
Zack <zholloway92@gmail.com>
2017-04-06 14:07:04 -0400

    first commmit. library class created

---
+++ LibraryAPI/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryAPI.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int YearPublished { get; set; }
+        public string Genre { get; set; }
+        public bool IsCheckedOut { get; set; }
+        public DateTime LastCheckedOutDate { get; set; }
+        public DateTime DueBackDate { get; set; }
+    }
+}
