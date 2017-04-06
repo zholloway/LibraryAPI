a3d9e04b88f3a15b6fe3ae6f5421800d74c00cb5
Zack <zholloway92@gmail.com>
2017-04-06 16:26:03 -0400

    getallbooks and addnewbook working

---
+++ LibraryAPI/Controllers/LibraryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LibraryAPI.Models;
+using System.Data.SqlClient;
+
+namespace LibraryAPI.Controllers
+{
+    public class LibraryController : ApiController
+    {
+        const string PathToLibraryDatabase = @"Server=localhost\SQLEXPRESS;Database=Library;Trusted_Connection=True;";
+
+        [HttpGet]
+        public IHttpActionResult GetAllBooks()
+        {
+            using (var connection = new SqlConnection(PathToLibraryDatabase))
+            {
+                return Ok(Book.GetAllBooks(connection));
+            }
+        }
+
+        [HttpPut]
+        public IHttpActionResult AddNewBook(Book newBook)
+        {
+            Book.AddNewBook(newBook, PathToLibraryDatabase);
+            return Ok("The book was successfully added.");           
+        }
+    }
+}
--- LibraryAPI/Models/Book.cs
+++ LibraryAPI/Models/Book.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.IO;
+using System.Data.SqlClient;
 
 namespace LibraryAPI.Models
 {
     public class Book
     {
+        public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public int YearPublished { get; set; }
@@ -14,5 +17,37 @@ namespace LibraryAPI.Models
         public bool IsCheckedOut { get; set; }
         public DateTime LastCheckedOutDate { get; set; }
         public DateTime DueBackDate { get; set; }
-    }
+
+        public static List<String> GetAllBooks(SqlConnection connection)
+        {
+            var bookList = new List<String>();
+
+            var cmd = new SqlCommand("SELECT Title, Author FROM Book", connection);
+            connection.Open();
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                bookList.Add($"{reader["Title"]} by {reader["Author"]}");
+            }
+            connection.Close();
+
+            return bookList;
+        }
+
+        public static void AddNewBook(Book newBook, string pathToDatabase)
+        {
+            using (var connection = new SqlConnection(pathToDatabase))
+            {
+                var cmd = new SqlCommand(@"INSERT INTO Book (Title, YearPublished) "
+                            + "Values (@Title, @YearPublished)", connection);
+
+                cmd.Parameters.AddWithValue("@Title", newBook.Title);
+                cmd.Parameters.AddWithValue("@YearPublished", newBook.YearPublished);
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }      
+        }
+    }   
 }
